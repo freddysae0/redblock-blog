2e81f4e2d11733ddbef5fda95dafc7618222202c
Freddy <freddysae00@gmail.com>
2025-11-22 21:54:44 +0100

    feat: paginate categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
     public function index()
     {
         return Inertia::render('Admin/Categories/Index', [
-            'categories' => Category::all(),
+            'categories' => Category::paginate(10),
         ]);
     }
 
