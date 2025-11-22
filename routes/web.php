e1a74eac81952df6a01a8cf927fba7b3b225951d
Freddy <freddysae00@gmail.com>
2025-11-22 18:21:40 +0100

    feat: Add initial web routes for home, dashboard, articles, categories, users, and comments.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Laravel\Fortify\Features;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\UserController;
 use App\Models\Article;
 
 Route::get('/', function () {
@@ -22,12 +23,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
-    Route::resource('articles', ArticleController::class)->except(['create', 'edit']);
-    Route::resource('categories', CategoryController::class)->except(['create', 'edit']);
+    Route::resource('articles', ArticleController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('users', UserController::class)->only(['index', 'destroy']);
+    Route::get('comments', [CommentController::class, 'index'])->name('comments.index');
 
     Route::post('articles/{article}/comments', [CommentController::class, 'store'])->name('articles.comments.store');
     Route::put('comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
     Route::delete('comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
