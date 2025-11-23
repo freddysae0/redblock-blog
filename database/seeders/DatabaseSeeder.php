ce018e1494e082d8a0a0a5a051be7384d1a309f2
Freddy <freddysae00@gmail.com>
2025-11-23 23:22:57 +0100

    feat: add initial database seeders for users, categories, articles, and comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,17 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::firstOrCreate(
-            ['email' => 'test@admin.com'],
-            [
-                'name' => 'Test Admin',
-                'is_mantainer' => true,
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ],
-        );
-
         $this->call([
+            UserSeeder::class,
             CategorySeeder::class,
             ArticleSeeder::class,
             CommentSeeder::class,
